d2b1e3091936e010579f5411579bfdb80cfddc2a
thsam <planetxz@hotmail.com>
2020-09-28 13:46:42 -0500

    initial code, links home

---
+++ encyclopedia/views.py
@@ -0,0 +1,24 @@
+import markdown2
+from django.shortcuts import render
+
+from . import util
+from django.urls import reverse
+from markdown2 import Markdown
+
+def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+
+def entry(request,entry):
+    markdowner= Markdown()
+    page=util.get_entry(entry) #entrega el md
+    if page is not None: 
+        return render(request,"encyclopedia/entry.html",{
+            "entry":markdowner.convert(page),
+            "entry_title":entry
+            
+        })
+
+
+
