0c8dd6dfad11b016472eeb274403ba9841c62db1
thsam <planetxz@hotmail.com>
2020-12-30 01:11:02 -0500

    production

--- wiki/settings.py
+++ wiki/settings.py
@@ -77,17 +77,14 @@ WSGI_APPLICATION = 'wiki.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-""" DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-} """
+}
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
+
 
 
 
